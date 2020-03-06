6849adea38e4d6396c40c104b3ee39b8a5dcd2ba
hossein.sho <amiramiry125@gmail.com>
2020-03-06 20:44:11 +0330

    before JSON

--- chatroom/message/admin.py
+++ chatroom/message/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Messages
+from .models import Messages,ConversationMember,Conversation
 
 
 @admin.register(Messages)
 
 class adminMessages(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ConversationMember)
+class adminConversationMember(admin.ModelAdmin):
+    pass
+
+@admin.register(Conversation)
+class adminConversation(admin.ModelAdmin):
     pass
--- chatroom/message/models.py
+++ chatroom/message/models.py
@@ -1,14 +1,42 @@
 from django.db import models
 from users.models import Users
+from django.contrib.auth.models import User
+import datetime
 
 #from users.model improt messages mishod zad o kareto bokoni
-# Create your models here.
+# Create your models here0.
+
+
+
+class Conversation(models.Model):
+    name = models.CharField(max_length=50)
+    is_group = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
+
+
+class ConversationMember(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    conversation = models.ForeignKey(Conversation,on_delete=models.CASCADE,null = True)
+    is_admin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.conversation
+
+
+
 class Messages(models.Model):
     text = models.TextField()
-    sender = models.ForeignKey(Users, on_delete = models.CASCADE,related_name='senders')
-    receiver = models.ForeignKey(Users, on_delete = models.CASCADE,related_name='receivers')
-    date  = models.DateTimeField(auto_now_add=True)
+    sender = models.ForeignKey(User, on_delete = models.CASCADE)
+    
+    conversation = models.ForeignKey(Conversation,on_delete=models.CASCADE,null = True)
+    
+    date  = models.DateTimeField(auto_now_add=True,null =True)
     status = models.IntegerField(default=1) #1:send 2:recieve 3:seen
 
     def __str__(self):
-        return '%s,%s,%s'%(self.text,self.sender, self.receiver)
+        return '%s,%s'  %(self.text,self.sender)
+
+
+ #u  = User.objects.get(username  ='king') 
